c2b133e52a9b63814ebcf7c9a6b2558e7d4ea2a5
xyingg0611 <xyingg2003@gmail.com>
2024-08-21 10:41:54 +0800

    Initial commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,11 +1,8 @@
 package com.example;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-
     public String getGreeting() {
-        return "Hello, World1!";
+        return "Hello, World!";
     }
 }
+
